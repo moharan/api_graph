ed86df34bb5caab4907192fac0bf1395875f4987
RangiBecerra <rangibecerra@gmail.com>
2017-07-05 12:26:40 -0400

    elimine una parte de codigo

--- js/main.js
+++ js/main.js
@@ -20,15 +20,6 @@ FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
 });
 
-{
-    status: 'connected',
-    authResponse: {
-        accessToken: '...',
-        expiresIn:'...',
-        signedRequest:'...',
-        userID:'...'
-    }
-}
 function checkLoginState() {
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
